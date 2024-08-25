9710e642e543831d9150e9b6c4987a0a5d4d6d81
Maddie T <mtimm90136@uat.edu>
2024-08-24 20:33:53 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+const form = document.querySelector('form');
+const input = document.querySelector('input');
+
+form.addEventListener('submit', function(event) {
+  event.preventDefault();
+
+  if (input.value.trim() === '') {
+    alert('Please fill out the input field!');
+  } else {
+    // Form submission logic goes here
+    form.submit();
+  }
+});
+
+function play() {
+  var audio = document.getElementById("audio");
+  audio.play();
+}
